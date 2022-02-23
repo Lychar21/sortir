f1960a1a5fb032d237d2bd698603f409abbfe550
Nicolas RISO <nicolas.riso13@gmail.com>
2022-02-23 06:23:29 +0100

    Commit Entity/Authentification/Security/Formulaire/Css

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_acceuil")
+     */
+    public function acceuil()
+    {
+        return $this->render('main/acceuil.html.twig');
+
+    }
+
+}
