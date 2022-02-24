950f4dace6469a9734a7186801a8b5791c2e0324
Nicolas RISO <nicolas.riso13@gmail.com>
2022-02-24 03:37:07 +0100

    commit CSS

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,13 +8,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main_acceuil")
+     * @Route("/", name="base")
+     */
+    public function base()
+    {
+        return $this->render('base.html.twig');
+
+    }
+
+    /**
+     * @Route("/acceuil", name="main_acceuil")
      */
     public function acceuil()
     {
         return $this->render('main/acceuil.html.twig');
 
     }
+    /**
+     * @Route("/monCompte", name="main_monCompte")
+     */
+    public function monCompte()
+    {
+        return $this->render('main/monCompte.html.twig');
+
+    }
+
 
 
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,7 +20,7 @@ class RegistrationFormType extends AbstractType
             ->add('prenom')
             ->add('email')
             ->add('telephone')
-            ->add('password', PasswordType::class, [
+            ->add('password',PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
